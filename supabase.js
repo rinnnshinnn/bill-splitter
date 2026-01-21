2a76daba9055abe159d328e1f99464af11acdb13
rinnnshinnn <rinnnshinnn@gmail.com>
2026-01-22 01:33:43 +0800

    Changed project url and anon public key

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "https://mmgeueznceaxxteyjxmi.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tZ2V1ZXpuY2VheHh0ZXlqeG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwMDU4MzIsImV4cCI6MjA4NDU4MTgzMn0.9zEryCvz3vsBbkyeYiUHjjR6zUw4c4FS6pbIjuTx_e0"
+  "https://hfeplzzpsppihcbbcylg.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmZXBsenpwc3BwaWhjYmJjeWxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwMTE1ODEsImV4cCI6MjA4NDU4NzU4MX0.iuRo3sUba3ly5so9C-ubgZtc6B4t_zE1B0IneW-miOo"
 );
